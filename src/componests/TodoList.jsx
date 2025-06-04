e77fe3c482a07ce60490917c1b220227b08982b9
Ronaldvillan <ron.villan@gmail.com>
2025-06-04 10:37:55 -0300

    Mover .gitattributes a la raíz del proyecto

--- src/componests/TodoList.jsx
+++ src/componests/TodoList.jsx
@@ -1,23 +1,21 @@
 // import React from 'react';
-// import { TodoItem } from "./TodoItem";
+// import { TodoItem } from './TodoItem';
 
 // export function TodoList({ todos, toggleTodo }) {
-//     const pendingTodos = todos.filter((todo) => !todo.completed); // Filtra las tareas pendientes
-
 //     return (
 //         <div className="card">
-//             {pendingTodos.length === 0 && (
-//                 <p className="no-tasks">No hay tareas pendientes</p> // Mostrar mensaje si no hay tareas pendientes
-//             )}
 //             <ul>
-//                 {todos.map((todo, index) => (
-//                     <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} index={index} />
-//                 ))}
+//                 {todos.length > 0 ? (
+//                     todos.map((todo, index) => (
+//                         <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} index={index} />
+//                     ))
+//                 ) : (
+//                     <li className="no-tasks-message">¡Todo listo! No hay tareas pendientes.</li>  // El mensaje solo aparece cuando la lista está vacía
+//                 )}
 //             </ul>
 //         </div>
 //     );
 // }
-
 
 import React from 'react';
 import { TodoItem } from './TodoItem';
@@ -31,12 +29,13 @@ export function TodoList({ todos, toggleTodo }) {
                         <TodoItem key={todo.id} todo={todo} toggleTodo={toggleTodo} index={index} />
                     ))
                 ) : (
-                    <li className="no-tasks-message">¡Todo listo! No hay tareas pendientes.</li>  // El mensaje solo aparece cuando la lista está vacía
+                    <li className="no-tasks-message">
+                        ¡Todo listo! No hay tareas pendientes.
+                    </li>
                 )}
             </ul>
         </div>
     );
 }
-
 
 
